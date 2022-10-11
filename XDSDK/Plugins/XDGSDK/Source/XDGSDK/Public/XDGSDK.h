64879a1a7e8941c8dbaa4d55eea9d4e60812ce3e
harryphone <harryphone@163.com>
2022-10-11 13:00:50 +0800

    feat: 版本号更新

--- XDSDK/Plugins/XDGSDK/Source/XDGSDK/Public/XDGSDK.h
+++ XDSDK/Plugins/XDGSDK/Source/XDGSDK/Public/XDGSDK.h
@@ -5,8 +5,8 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-#define XDUESDK_VERSION_NUMBER "60503001"
-#define XDUESDK_VERSION "6.5.3"
+#define XDUESDK_VERSION_NUMBER "60600001"
+#define XDUESDK_VERSION "6.6.0"
 
 
 class XDGSDK_API FXDGSDKModule : public IModuleInterface

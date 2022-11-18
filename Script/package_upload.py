1d87cf2da41197a4231cfda9dad0f0636d8dcc03
harryphone <harryphone@163.com>
2022-11-18 16:51:00 +0800

    feat：slack message update -win

--- Script/package_upload.py
+++ Script/package_upload.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
     products = build.parse_need_products(" ".join(sys.argv[1:]))
     if len(products) == 0:
         exit(0)
-    message = "XDSDK-UE Build"
+    message = "*XDSDK-UE Build*"
     if "CI_RUNNER_TAGS" in os.environ:
-        message = message + f" ({os.environ['CI_RUNNER_TAGS']})"
+        message = message + f" `{os.environ['CI_RUNNER_TAGS']}`"
     if "CI_JOB_URL" in os.environ:
         message = message + f"\n<{os.environ['CI_JOB_URL']}|PackageStart>"
     if "CI_COMMIT_AUTHOR" in os.environ:
-        message = message + f"\n{os.environ['CI_COMMIT_AUTHOR']}"
+        message = message + f"\n>{os.environ['CI_COMMIT_AUTHOR']}"
 
     thread_id = slack_bot.sendMessage(message)
     for platform_str in products:

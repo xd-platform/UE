e53e71ad87bedf88230375b9f8664db1dcf5c021
harryphone <harryphone@163.com>
2022-11-18 16:59:17 +0800

    feat：脚本打印更新

--- Script/check_packages.py
+++ Script/check_packages.py
@@ -7,13 +7,14 @@ import slack_bot
 if __name__ == '__main__':
     # 解析需要的平台
     products = build.parse_need_products("-all")
-    message = "XDSDK-UE Check"
+
+    message = "*XDSDK-UE Check*"
     if "CI_RUNNER_TAGS" in os.environ:
-        message = message + f" ({os.environ['CI_RUNNER_TAGS']})"
+        message = message + f" `{os.environ['CI_RUNNER_TAGS']}`"
+    if "CI_COMMIT_AUTHOR" in os.environ:
+        message = message + f"\n>{os.environ['CI_COMMIT_AUTHOR']}"
     if "CI_JOB_URL" in os.environ:
         message = message + f"\n<{os.environ['CI_JOB_URL']}|PackageStart>"
-    if "CI_COMMIT_AUTHOR" in os.environ:
-        message = message + f"\n{os.environ['CI_COMMIT_AUTHOR']}"
 
     thread_id = slack_bot.sendMessage(message)
     for platform_str in products:
